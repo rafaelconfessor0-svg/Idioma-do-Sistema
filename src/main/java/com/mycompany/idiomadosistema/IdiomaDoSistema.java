2513f8355bd92dfd0556b578a85b44c26a8515e6
rafaelconfessor0-svg <rafaelconfessor0@gmail.com>
2026-08-03 19:30:08 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.idiomadosistema;
+
+/**
+ *
+ * @author rafael
+ */
+public class IdiomaDoSistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
